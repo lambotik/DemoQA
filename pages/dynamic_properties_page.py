ae039e3f4f690377d42cac5fd4319c9f90daca81
lambotik <lambotik1988@gmail.com>
2023-01-10 21:25:49 +0300

    Update: elements tests added: class TestDynamicPropertiesPage added tests: test_dynamic_properties, test_appear_button, test_enable_button

--- pages/dynamic_properties_page.py
+++ pages/dynamic_properties_page.py
@@ -4,34 +4,39 @@ from selenium.common import TimeoutException
 
 from locators.elements_page_locators import DynamicPropertiesPageLocators
 from pages.base_page import BasePage
+from utilities.logger import Logger
 
 
 class DynamicPropertiesPage(BasePage):
     locators = DynamicPropertiesPageLocators()
 
-    # Actions
+    def check_enable_button(self):
+        Logger.add_start_step(method='check_enable_button')
+        try:
+            print('Checking if an element is enable')
+            self.elements_is_clickable(self.locators.ENABLE_AFTER_BUTTON).click()
+            Logger.add_end_step(url=self.driver.current_url, method='check_enable_button')
+        except TimeoutException:
+            return False
+        return True
+
+    def check_appear_button(self):
+        Logger.add_start_step(method='check_appear_button')
+        try:
+            print('Checking if an element is visible')
+            self.element_is_visible(self.locators.VISIBLE_AFTER_BUTTON)
+            Logger.add_end_step(url=self.driver.current_url, method='check_appear_button')
+        except TimeoutException:
+            return False
+        return True
 
     def check_changed_of_color(self):
+        Logger.add_start_step(method='check_changed_of_color')
         color_button = self.element_is_present(self.locators.COLOR_CHANGE_BUTTON)
         color_button_before = color_button.value_of_css_property('color')
         print('Initial button color: ', color_button_before)
         time.sleep(5)
         color_button_after = color_button.value_of_css_property('color')
         print('Button color after clicking: ', color_button_after)
+        Logger.add_end_step(url=self.driver.current_url, method='check_changed_of_color')
         return color_button_before, color_button_after
-
-    def check_appear_button(self):
-        try:
-            print('Checking if an element is visible')
-            self.element_is_visible(self.locators.VISIBLE_AFTER_BUTTON)
-        except TimeoutException:
-            return False
-        return True
-
-    def check_enable_button(self):
-        try:
-            print('Checking if an element is enable')
-            self.elements_is_clickable(self.locators.ENABLE_AFTER_BUTTON).click()
-        except TimeoutException:
-            return False
-        return True

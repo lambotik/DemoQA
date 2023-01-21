4137f4e9facaa041f63ca8996a4cff853185deb9
lambotik <lambotik1988@gmail.com>
2023-01-22 02:39:59 +0300

    Update: added: class TestAlertFrameWindowsPage added: class AlertFrameWindowsPageLocators added tests: test_new_tab, test_new_window_page added methods: check_new_tab_page, check_new_window_page

--- locators/alert_frame_window_page_locators.py
+++ locators/alert_frame_window_page_locators.py
@@ -2,5 +2,8 @@ from selenium.webdriver.common.by import By
 
 
 class AlertFrameWindowsPageLocators:
-    USER_ICON = (By.XPATH, '//div[@id="header-photo"]')
-    SETTINGS = (By.XPATH, '//*[@id="Layer_2"]')
+    NEW_TAB_BUTTON = (By.XPATH, '//button[@id="tabButton"]')
+    NEW_WINDOW_BUTTON = (By.XPATH, '//button[@id="windowButton"]')
+    NEW_WINDOW_MESSAGE_BUTTON = (By.XPATH, '//button[@id="messageWindowButton"]')
+    SAMPLE_PAGE = (By.XPATH, '//h1[@id="sampleHeading"]')
+    NEW_WINDOW_MESSAGE = (By.XPATH, '/html/body/text()')

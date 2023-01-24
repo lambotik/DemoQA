7c2409bd250a7ce2a3e7b309dc018c7447832c7f
lambotik <lambotik1988@gmail.com>
2023-01-25 01:42:16 +0300

    Update: added generator: generated_color added class: TestAutoComplete, AutoCompletePage, AutoCompletePageLocators added tests: test_fill_multi_auto_complete, test_remove_all_colors, test_fill_single_complete added methods: fill_input_multi, remove_value_from_multi, check_color_in_multi, fill_input_single

--- locators/widgets_locators.py
+++ locators/widgets_locators.py
@@ -9,3 +9,13 @@ class AccordianPageLocators:
     SECOND_TAB_TEXT = (By.XPATH, '//div[@id="section2Content"]/p')
     THIRD_TAB = (By.XPATH, '//div[@id="section3Heading"]')
     THIRD_TAB_TEXT = (By.XPATH, '//div[@id="section3Content"]/p')
+
+
+class AutoCompletePageLocators:
+    # Auto complete
+    INPUT_MULTI = (By.XPATH, '//input[@id="autoCompleteMultipleInput"]')
+    LIST_MULTI_VALUE = (By.XPATH, '//div[@class="css-1rhbuit-multiValue auto-complete__multi-value"]')
+    CLOSE_MULTI_VALUE_ELEMENT = (
+        By.CSS_SELECTOR, 'div[class="css-1rhbuit-multiValue auto-complete__multi-value"] svg path')
+    INPUT_SINGLE = (By.XPATH, '//input[@id="autoCompleteSingleInput"]')
+    SINGLE_CONTAINER = (By.XPATH, '//div[@class="auto-complete__single-value css-1uccc91-singleValue"]')
--- tests/test_widgets.py
+++ tests/test_widgets.py
@@ -1,4 +1,4 @@
-from pages.Widgets.widgets_page import AccordianPage
+from pages.Widgets.widgets_page import AccordianPage, AutoCompletePage
 
 
 class TestWidgetsPage:
@@ -13,3 +13,25 @@ class TestWidgetsPage:
             assert firs_tab_title == 'What is Lorem Ipsum?' and first_tab_content > 0
             assert second_tab_title == 'Where does it come from?' and second_tab_content > 0
             assert third_tab_title == 'Why do we use it?' and third_tab_content > 0
+
+    class TestAutoComplete:
+
+        def test_fill_multi_auto_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            color = auto_complete_page.fill_input_multi()
+            colors = auto_complete_page.check_color_in_multi()
+            assert color == colors, 'The added colors are missing in the input'
+
+        def test_remove_all_colors(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            auto_complete_page.fill_input_multi()
+            count_value_before, count_value_after = auto_complete_page.remove_value_from_multi()
+            assert count_value_before != count_value_after, 'Values did not deleted'
+
+        def test_fill_single_complete(self, driver):
+            auto_complete_page = AutoCompletePage(driver, 'https://demoqa.com/auto-complete')
+            auto_complete_page.open()
+            color, color_result = auto_complete_page.fill_input_single()
+            assert color == color_result, 'The added color are missing in the input'

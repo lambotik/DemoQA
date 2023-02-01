e4f50164b0f1bd7630fe2e8bc30d4ceea8ef2c66
lambotik <lambotik1988@gmail.com>
2023-02-01 23:12:04 +0300

    Update Interactions: added class: TestSelectable, SelectablePage, SelectablePageLocators. added tests: test_check_change_list_order, test_check_change_grid_order. added Methods: random_selection_and_return_active_and_inactive_elements, grid_random_selection_and_return_active_and_inactive_elements

--- pages/Interactions/interactions_page.py
+++ pages/Interactions/interactions_page.py
@@ -1,6 +1,6 @@
 import random
 
-from locators.interactions_page_locators import SortablePageLocators
+from locators.interactions_page_locators import SortablePageLocators, SelectablePageLocators
 from pages.base_page import BasePage
 from utilities.logger import Logger
 
@@ -39,3 +39,46 @@ class SortablePage(BasePage):
         print(order_after)
         Logger.add_end_step(url=self.driver.current_url, method='change_grid_order')
         return order_before, order_after
+
+
+class SelectablePage(BasePage):
+    locators = SelectablePageLocators()
+
+    def get_list_elements(self, elements):
+        item_list = self.elements_are_visible(elements)
+        data = []
+        for item in item_list:
+            data.append(item)
+        return data
+
+    def random_selection_and_return_active_and_inactive_elements(self):
+        Logger.add_start_step(method='random_selection_and_return_active_and_inactive_elements')
+        self.element_is_visible(self.locators.LIST).click()
+        list_elements = self.get_list_elements(self.locators.LIST_ITEMS_LIST_TAB)
+        for i in range(len(list_elements) - 1):
+            random.choice(list_elements).click()
+        active_element = self.elements_are_visible(self.locators.LIST_ACTIVE_ITEMS_LIST_TAB)
+        list_active_elements = [item.text for item in active_element]
+        print('List of active elements', list_active_elements)
+        not_selected_elements = self.get_list_elements(self.locators.LIST_ITEMS_LIST_TAB)
+        list_not_selected_elements = [item.text for item in not_selected_elements]
+        print('List of inactive elements: ', list_not_selected_elements)
+        Logger.add_end_step(url=self.driver.current_url,
+                            method='random_selection_and_return_active_and_inactive_elements')
+        return list_active_elements, list_not_selected_elements
+
+    def grid_random_selection_and_return_active_and_inactive_elements(self):
+        Logger.add_start_step(method='grid_random_selection_and_return_active_and_inactive_elements')
+        self.element_is_visible(self.locators.GRID).click()
+        list_elements = self.get_list_elements(self.locators.LIST_ITEMS_GRID_TAB)
+        for i in range(len(list_elements) - 1):
+            random.choice(list_elements).click()
+        active_element = self.elements_are_visible(self.locators.LIST_ACTIVE_ITEMS_GRID_TAB)
+        list_active_elements = [item.text for item in active_element]
+        print('List of active elements', list_active_elements)
+        not_selected_elements = self.get_list_elements(self.locators.LIST_ITEMS_GRID_TAB)
+        list_not_selected_elements = [item.text for item in not_selected_elements]
+        print('List of inactive elements: ', list_not_selected_elements)
+        Logger.add_end_step(url=self.driver.current_url,
+                            method='grid_random_selection_and_return_active_and_inactive_elements')
+        return list_active_elements, list_not_selected_elements

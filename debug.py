21525e2c2382de062e7d7255c3a1bd7acc73c305
lambootik <lambotik1988@gmail.com>
2023-03-05 21:50:08 +0300

    Update: Fixed bug with allure report

--- debug.py
+++ debug.py
@@ -1,12 +1,6 @@
-import re
+from datetime import datetime
+from pathlib import Path
 
-position = ['position: relative; left: 31px; top: 0px;'][0].split(';')
-left = position[1]
-top = position[2]
-l = re.findall(r'\d+', left)
-t = re.findall(r'\d+', top)
-le = [int(i) for i in l][0]
-to = [int(i) for i in t][0]
-print(le)
-print(to)
+LOGS_DIR = Path(__file__).parent.joinpath('logs')
+print(LOGS_DIR)
 

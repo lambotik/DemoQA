2d147fbdbfcc8ed41e21c5b266396de4dae33c6d
QA/AQA Python Dzmitry Charukha <91555504+lambotik@users.noreply.github.com>
2023-09-20 02:26:28 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,48 @@
 from datetime import datetime
+import os
 import allure
+
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.common import UnexpectedAlertPresentException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture()
+@pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    start_time = str(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+    full_test_name = os.environ.get("PYTEST_CURRENT_TEST").split('::')
+    test_name = full_test_name[-1]
+    test_name = test_name.replace(' (setup)', '')
+    print(f'\nStart Test: <{test_name}> {start_time}')
+    options = Options()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
     driver.maximize_window()
-    driver.implicitly_wait(10)
     yield driver
-    attach = driver.get_screenshot_as_png()
-    allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
+    try:
+        attach = driver.get_screenshot_as_png()
+        allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
+    except UnexpectedAlertPresentException:
+        attach = driver.get_screenshot_as_png()
+        allure.attach(attach, name=f"Screenshot {datetime.today()}", attachment_type=allure.attachment_type.PNG)
+    finish_time = str(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
+    print(f'Test Finish: {finish_time}')
     driver.quit()
+
+
+@pytest.fixture()
+def set_up():
+    print('Start Test')
+    yield
+    print('Finish Test')
+
+
+@pytest.fixture(scope='module')
+def set_group():
+    print('Start System')
+    yield
+    print('Exit System')
